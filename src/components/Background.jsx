9ae7de840510188bc65d3b8dee31e66502cf0e08
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-12 16:57:56 +0700

    Refactor BackgroundComponent structure and styles for improved layout and animation

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,3 +1,7 @@
+import leaf_01 from '../assets/leaf_01.png'
+import leaf_02 from '../assets/leaf_02.png'
+import leaf_03 from '../assets/leaf_03.png'
+import leaf_04 from '../assets/leaf_04.png'
 import background from '../assets/background.jpg'
 import trees from '../assets/trees.png'
 import girl from '../assets/girl.png'
@@ -7,12 +11,26 @@ const BackgroundComponent = (props) => {
     const { children } = props;
 
     return (
-        <div>
+        <section>
+            <div className="leaves">
+                <div className="set">
+                    <div><img src={leaf_01} /></div>
+                    <div><img src={leaf_02} /></div>
+                    <div><img src={leaf_03} /></div>
+                    <div><img src={leaf_04} /></div>
+                    <div><img src={leaf_01} /></div>
+                    <div><img src={leaf_02} /></div>
+                    <div><img src={leaf_03} /></div>
+                    <div><img src={leaf_04} /></div>
+                </div>
+            </div>
+
             <img src={background} alt="Background" className="bg" />
             <img src={trees} alt="Trees" className="trees" />
             <img src={girl} alt="Girl" className="girl" />
+
             {children}
-        </div>
+        </section>
     )
 }
 
