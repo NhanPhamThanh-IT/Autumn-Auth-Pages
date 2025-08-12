14ddcc67a8c7a970c2fe80a58c0ac47a0d359a1a
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-08-12 16:39:14 +0700

    Add girl image and animation to BackgroundComponent

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,5 +1,6 @@
 import background from '../assets/background.jpg'
 import trees from '../assets/trees.png'
+import girl from '../assets/girl.png'
 import '../styles/background.css'
 
 const BackgroundComponent = (props) => {
@@ -9,7 +10,7 @@ const BackgroundComponent = (props) => {
         <div>
             <img src={background} alt="Background" className="bg" />
             <img src={trees} alt="Trees" className="trees" />
-
+            <img src={girl} alt="Girl" className="girl" />
             {children}
         </div>
     )
